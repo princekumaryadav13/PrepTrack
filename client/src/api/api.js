ec34428bbd5cb71d1dd46f483e0da855155bdcfc
prince <py964830@gmail.com>
2026-09-22 22:50:29 +0530

    Prepare PrepTrack for deployment

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,5 +1,7 @@
-const API_URL = "http://localhost:5000/api";
+// const API_URL = "http://localhost:5000/api";
 
+ const API_URL = import.meta.env.VITE_API_URL;
+ 
 const getToken = () => {
     return localStorage.getItem("token");
 };
--- server/server.js
+++ server/server.js
@@ -24,7 +24,12 @@ connectDB();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 
 // Routes
 app.use("/api/auth", authRoutes);
